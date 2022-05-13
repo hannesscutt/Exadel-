4a5b4010e4ec56e8ad9a6cd9c30d8877356dfc41
Hannes Scutt <johannesscutt@gmail.com>
2022-05-13 03:43:35 -0400

    added models

---
+++ ExadelTimeTrackingSystem/ExadelTimeTrackingSystem.Data/Models/Project.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExadelTimeTrackingSystem.Data.Models
+{
+    public class Project
+    {
+        public string _id { get; set; }
+
+        public int[] Activities { get; set; }
+
+        public string Name { get; set; }
+
+        public string ApproverID { get; set; }
+
+        public string[] EmployeeIDs { get; set; }
+    }
+}
