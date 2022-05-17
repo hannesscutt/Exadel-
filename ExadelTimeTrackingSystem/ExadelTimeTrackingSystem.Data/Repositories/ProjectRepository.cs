8315e6e8359be4b1fbc26376db6cb784e04ffce9
Hannes Scutt <johannesscutt@gmail.com>
2022-05-17 11:05:36 -0400

    removed excess code from projectrepository

--- ExadelTimeTrackingSystem/ExadelTimeTrackingSystem.Data/Repositories/ProjectRepository.cs
+++ ExadelTimeTrackingSystem/ExadelTimeTrackingSystem.Data/Repositories/ProjectRepository.cs
@@ -3,17 +3,12 @@
     using ExadelTimeTrackingSystem.Data.Configuration.Abstract;
     using ExadelTimeTrackingSystem.Data.Models;
     using ExadelTimeTrackingSystem.Data.Repositories.Abstract;
-    using MongoDB.Driver;
 
     public class ProjectRepository : MongoRepository<Project>, IProjectRepository
     {
-        private readonly IMongoDatabase _database;
-
         public ProjectRepository(IMongoDbSettings settings)
             : base(settings)
         {
-            var client = new MongoClient(settings.ConnectionString);
-            _database = client.GetDatabase(new MongoUrlBuilder(settings.ConnectionString).DatabaseName);
         }
     }
 }
