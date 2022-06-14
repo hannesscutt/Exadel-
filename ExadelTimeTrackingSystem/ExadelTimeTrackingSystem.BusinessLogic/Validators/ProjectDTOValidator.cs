04c6a05b837b201349057a4e7c19033bb995e239
Hannes Scutt <johannesscutt@gmail.com>
2022-06-14 00:28:12 -0400

    added project validator

---
+++ ExadelTimeTrackingSystem/ExadelTimeTrackingSystem.BusinessLogic/Validators/ProjectDTOValidator.cs
@@ -0,0 +1,27 @@
+﻿namespace ExadelTimeTrackingSystem.Data.Validators
+{
+    using ExadelTimeTrackingSystem.BusinessLogic.DTOs;
+    using FluentValidation;
+
+    public class ProjectDTOValidator : AbstractValidator<ProjectDTO>
+    {
+        private readonly IUserService _userService;
+        private CreateProjectDTOValidator createProjectDtoValidator;
+
+        public ProjectDTOValidator(IUserService userService)
+        {
+            _userService = userService;
+            createProjectDtoValidator = new CreateProjectDTOValidator(_userService);
+            createProjectDtoValidator.ConfigureRules();
+            ConfigureRules();
+        }
+
+        public void ConfigureRules()
+        {
+            RuleFor(p => p.Id)
+                   .NotEmpty()
+                   .MustAsync(_userService.ExistAsync)
+                   .WithMessage(Constants.Validation.ID_DOES_NOT_EXIST);
+        }
+    }
+}
